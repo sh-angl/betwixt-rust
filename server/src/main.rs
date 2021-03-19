b11d65b0949365f10df626e3532c3ddd04c361c7
oofsauce <alanpanayotov@gmail.com>
2021-03-19 15:47:17 +0000

    fix server incoming stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -19,21 +19,20 @@ use std::{
     net::SocketAddr
 };
 
-use futures::prelude::*;
+use futures::{channel::mpsc::unbounded, prelude::*};
 
 
 async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr) -> Result<(), TsError>{
     println!("Incoming TCP connection from: {}", addr);
-
     let ws_stream = tokio_ts::accept_async(raw_stream).await.expect("bro");
+    println!("WS connection established with {}", addr);
 
-    
+    // let (tx, rx) = unbounded();
     let (mut outgoing, incoming) = ws_stream.split();
 
     outgoing.send(Message::Ping(vec!())).await?;
-
-    println!("assigning the callback");
-    let _thing  = incoming.try_for_each(|msg| async move{
+    
+    let broadcast_incoming = incoming.try_for_each(|msg| async move{
         println!("running callback");
         match msg{
             Message::Ping(_) => {println!("Pinged by client");
@@ -45,6 +44,9 @@ async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr) -> Result<()
 
         Ok(())
     });
+    broadcast_incoming.await;
+
+    // future::select(broadcast_incoming, future2).await;
 
     Ok(())
     
@@ -56,7 +58,7 @@ async fn main() -> Result<(), IoError>{
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
-
+    
 
     let try_socket = TcpListener::bind(&addr).await;
     let listener = try_socket.expect("Failed to bind");
